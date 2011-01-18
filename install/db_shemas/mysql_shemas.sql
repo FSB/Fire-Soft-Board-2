eb363f30becf6524b984bcd161e4b5eb600d07d0
Axel Fontana <axel.fontana@neocyd.com>
2011-01-18 15:40:07 +0000

    Correction dans le schéma MySQL Type=MyISAM devient Engine=MyISAM

--- install/db_shemas/mysql_shemas.sql
+++ install/db_shemas/mysql_shemas.sql
@@ -4,7 +4,7 @@ CREATE TABLE fsb2_auths (
   auth_level tinyint(4) NOT NULL default '0',
   auth_begin tinyint(4) NOT NULL default '0',
   PRIMARY KEY  (auth_name)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_ban;
 CREATE TABLE fsb2_ban (
@@ -16,7 +16,7 @@ CREATE TABLE fsb2_ban (
   ban_cookie tinyint(4) NOT NULL,
   PRIMARY KEY  (ban_id),
   KEY ban_length (ban_length)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_bots;
 CREATE TABLE fsb2_bots (
@@ -26,7 +26,7 @@ CREATE TABLE fsb2_bots (
   bot_agent varchar(255) NOT NULL,
   bot_last int(11) NOT NULL,
   PRIMARY KEY  (bot_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_cache;
 CREATE TABLE fsb2_cache (
@@ -36,7 +36,7 @@ CREATE TABLE fsb2_cache (
   cache_time int(11) NOT NULL,
   PRIMARY KEY  (cache_hash),
   KEY cache_type (cache_type)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_calendar;
 CREATE TABLE fsb2_calendar (
@@ -50,7 +50,7 @@ CREATE TABLE fsb2_calendar (
   c_view tinyint(4) NOT NULL,
   PRIMARY KEY  (c_id),
   KEY c_begin (c_begin, c_end)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_censor;
 CREATE TABLE fsb2_censor (
@@ -59,14 +59,14 @@ CREATE TABLE fsb2_censor (
   censor_replace varchar(255) NOT NULL default '',
   censor_regexp tinyint(4) NOT NULL default '0',
   PRIMARY KEY  (censor_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_config;
 CREATE TABLE fsb2_config (
   cfg_name varchar(255) NOT NULL default '',
   cfg_value varchar(255) NOT NULL default '',
   PRIMARY KEY  (cfg_name)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_config_handler;
 CREATE TABLE fsb2_config_handler (
@@ -77,7 +77,7 @@ CREATE TABLE fsb2_config_handler (
   cfg_args text NOT NULL,
   cfg_type varchar(255) NOT NULL default '',
   KEY cfg_cat_subcat (cfg_cat, cfg_subcat)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_forums;
 CREATE TABLE fsb2_forums (
@@ -113,7 +113,7 @@ CREATE TABLE fsb2_forums (
   f_color varchar(255) NOT NULL default '',
   PRIMARY KEY  (f_id),
   KEY f_right_left (f_left, f_right)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_fsbcode;
 CREATE TABLE fsb2_fsbcode (
@@ -134,7 +134,7 @@ CREATE TABLE fsb2_fsbcode (
   fsbcode_order int(11) NOT NULL,
   PRIMARY KEY  (fsbcode_id),
   KEY fsbcode_tag (fsbcode_tag)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_groups;
 CREATE TABLE fsb2_groups (
@@ -149,7 +149,7 @@ CREATE TABLE fsb2_groups (
   g_rank mediumint(9) NOT NULL default '0',
   PRIMARY KEY  (g_id),
   KEY g_type (g_type)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_groups_auth;
 CREATE TABLE fsb2_groups_auth (
@@ -168,7 +168,7 @@ CREATE TABLE fsb2_groups_auth (
   ga_create_global_announce tinyint(4) NOT NULL default '0',
   ga_answer_global_announce tinyint(4) NOT NULL default '0',
   PRIMARY KEY  (g_id,f_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_groups_users;
 CREATE TABLE fsb2_groups_users (
@@ -176,7 +176,7 @@ CREATE TABLE fsb2_groups_users (
   u_id int(11) NOT NULL default '0',
   gu_status tinyint(4) NOT NULL default '0',
   PRIMARY KEY  (g_id,u_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_langs;
 CREATE TABLE fsb2_langs (
@@ -184,7 +184,7 @@ CREATE TABLE fsb2_langs (
   lang_key varchar(100) NOT NULL default '',
   lang_value text NOT NULL,
   PRIMARY KEY  (lang_name,lang_key)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_logs;
 CREATE TABLE fsb2_logs (
@@ -201,7 +201,7 @@ CREATE TABLE fsb2_logs (
   PRIMARY KEY  (log_id),
   KEY log_type (log_type),
   KEY log_user (log_user)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_menu_admin;
 CREATE TABLE fsb2_menu_admin (
@@ -212,7 +212,7 @@ CREATE TABLE fsb2_menu_admin (
   page_order smallint(5) NOT NULL default '0',
   page_icon varchar(255),
   KEY cat_page_order (cat_order, page_order)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_mods;
 CREATE TABLE fsb2_mods (
@@ -227,7 +227,7 @@ CREATE TABLE fsb2_mods (
   mod_type tinyint(4) NOT NULL default '0',
   KEY mod_type (mod_type),
   UNIQUE mod_name (mod_name)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_mp;
 CREATE TABLE fsb2_mp (
@@ -245,7 +245,7 @@ CREATE TABLE fsb2_mp (
   PRIMARY KEY  (mp_id),
   KEY mp_from (mp_from),
   KEY mp_to (mp_to)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_mp_blacklist;
 CREATE TABLE fsb2_mp_blacklist (
@@ -255,7 +255,7 @@ CREATE TABLE fsb2_mp_blacklist (
   PRIMARY KEY  (blacklist_id),
   KEY blacklist_to_id (blacklist_to_id),
   KEY blacklist_from_id (blacklist_from_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_notify;
 CREATE TABLE fsb2_notify (
@@ -268,7 +268,7 @@ CREATE TABLE fsb2_notify (
   notify_try tinyint(4) NOT NULL,
   PRIMARY KEY  (notify_id),
   KEY notify_time (notify_time)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_poll;
 CREATE TABLE fsb2_poll (
@@ -277,7 +277,7 @@ CREATE TABLE fsb2_poll (
   poll_total_vote int(11) NOT NULL default '0',
   poll_max_vote tinyint(4) NOT NULL default '0',
   PRIMARY KEY  (t_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_poll_options;
 CREATE TABLE fsb2_poll_options (
@@ -287,7 +287,7 @@ CREATE TABLE fsb2_poll_options (
   poll_opt_total int(11) NOT NULL default '0',
   PRIMARY KEY  (poll_opt_id),
   KEY t_id (t_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_poll_result;
 CREATE TABLE fsb2_poll_result (
@@ -295,7 +295,7 @@ CREATE TABLE fsb2_poll_result (
   t_id int(11) NOT NULL default '0',
   KEY t_id (t_id),
   KEY poll_result_u_id (poll_result_u_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_portail_config;
 CREATE TABLE fsb2_portail_config (
@@ -305,7 +305,7 @@ CREATE TABLE fsb2_portail_config (
   portail_functions varchar(255) NOT NULL,
   portail_args text NOT NULL,
   portail_type varchar(255) NOT NULL
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_portail_module;
 CREATE TABLE fsb2_portail_module (
@@ -315,7 +315,7 @@ CREATE TABLE fsb2_portail_module (
   pm_activ tinyint(4) NOT NULL default '0',
   KEY pm_order (pm_order),
   UNIQUE pm_name (pm_name)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_posts;
 CREATE TABLE fsb2_posts (
@@ -339,7 +339,7 @@ CREATE TABLE fsb2_posts (
   KEY f_per_user (u_id, f_id),
   KEY t_per_user (u_id, f_id, t_id),
   FULLTEXT KEY p_text (p_text)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_posts_abuse;
 CREATE TABLE fsb2_posts_abuse (
@@ -358,7 +358,7 @@ CREATE TABLE fsb2_posts_abuse (
   KEY u_id (u_id),
   KEY pa_mp_id (pa_mp_id),
   KEY pa_parent (pa_parent)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_process;
 CREATE TABLE fsb2_process (
@@ -368,7 +368,7 @@ CREATE TABLE fsb2_process (
   process_function varchar(255) NOT NULL,
   process_step_minimum int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY  (process_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_profil_fields;
 CREATE TABLE fsb2_profil_fields (
@@ -387,7 +387,7 @@ CREATE TABLE fsb2_profil_fields (
   pf_output varchar(255) NOT NULL default '',
   PRIMARY KEY  (pf_id),
   KEY pf_order (pf_order)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_ranks;
 CREATE TABLE fsb2_ranks (
@@ -399,7 +399,7 @@ CREATE TABLE fsb2_ranks (
   rank_color varchar(255) NOT NULL default '',
   PRIMARY KEY  (rank_id),
   KEY rank_quota (rank_quota)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_search_match;
 CREATE TABLE fsb2_search_match (
@@ -408,7 +408,7 @@ CREATE TABLE fsb2_search_match (
   is_title tinyint(4) NOT NULL default '0',
   KEY word_id (word_id),
   KEY p_id (p_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_search_word;
 CREATE TABLE fsb2_search_word (
@@ -416,7 +416,7 @@ CREATE TABLE fsb2_search_word (
   word_content varchar(40) NOT NULL default '',
   PRIMARY KEY (word_id),
   UNIQUE word_content (word_content)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_sessions;
 CREATE TABLE fsb2_sessions (
@@ -437,7 +437,7 @@ CREATE TABLE fsb2_sessions (
   PRIMARY KEY  (s_sid),
   KEY s_id (s_id),
   KEY s_time (s_time)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_smilies;
 CREATE TABLE fsb2_smilies (
@@ -447,7 +447,7 @@ CREATE TABLE fsb2_smilies (
   smiley_name varchar(255) NOT NULL default '',
   smiley_order mediumint(9) NOT NULL default '0',
   PRIMARY KEY  (smiley_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_smilies_cat;
 CREATE TABLE fsb2_smilies_cat (
@@ -456,7 +456,7 @@ CREATE TABLE fsb2_smilies_cat (
   cat_order smallint(5) NOT NULL,
   PRIMARY KEY  (cat_id),
   KEY cat_order (cat_order)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_sub_procedure;
 CREATE TABLE fsb2_sub_procedure (
@@ -465,7 +465,7 @@ CREATE TABLE fsb2_sub_procedure (
   procedure_source text NOT NULL,
   procedure_auth tinyint(4) NOT NULL default '2',
   PRIMARY KEY (procedure_id) 
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_topics;
 CREATE TABLE fsb2_topics (
@@ -493,7 +493,7 @@ CREATE TABLE fsb2_topics (
   KEY f_id (f_id),
   KEY t_last_p_time (t_last_p_time),
   FULLTEXT KEY t_title (t_title)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_topics_notification;
 CREATE TABLE fsb2_topics_notification (
@@ -501,7 +501,7 @@ CREATE TABLE fsb2_topics_notification (
   t_id int(11) NOT NULL default '0',
   tn_status tinyint(4) NOT NULL default '0',
   PRIMARY KEY t_u_id (u_id,t_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_topics_read;
 CREATE TABLE fsb2_topics_read (
@@ -510,7 +510,7 @@ CREATE TABLE fsb2_topics_read (
   p_id int(11) NOT NULL default '0',
   tr_last_time int(11) NOT NULL default '0',
   PRIMARY KEY t_u_id (u_id,t_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_upload;
 CREATE TABLE fsb2_upload (
@@ -525,7 +525,7 @@ CREATE TABLE fsb2_upload (
   upload_auth tinyint(4) NOT NULL default '0',
   PRIMARY KEY  (upload_id),
   KEY u_id (u_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_users;
 CREATE TABLE fsb2_users (
@@ -585,7 +585,7 @@ CREATE TABLE fsb2_users (
   PRIMARY KEY  (u_id),
   KEY u_nickname (u_nickname),
   KEY u_birthday (u_birthday)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_users_password;
 CREATE TABLE fsb2_users_password (
@@ -598,19 +598,19 @@ CREATE TABLE fsb2_users_password (
   PRIMARY KEY  (u_id),
   KEY u_login (u_login),
   UNIQUE u_autologin_key (u_autologin_key)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_users_contact;
 CREATE TABLE fsb2_users_contact (
   u_id int(11) NOT NULL default '0',
   PRIMARY KEY  (u_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_users_personal;
 CREATE TABLE fsb2_users_personal (
   u_id int(11) NOT NULL default '0',
   PRIMARY KEY  (u_id)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS fsb2_warn;
 CREATE TABLE fsb2_warn (
@@ -625,5 +625,5 @@ CREATE TABLE fsb2_warn (
   PRIMARY KEY  (warn_id),
   KEY u_id (u_id),
   KEY warn_time (warn_time)
-) Type=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) Engine=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
