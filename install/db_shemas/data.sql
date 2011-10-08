bdf72a086d70763514086cc2fbd6ea9acbcea65e
Axel Fontana <fontana.axel@gmail.com>
2011-10-08 17:53:43 +0200

    FSB_F_T#9468 Changement du nom du groupe fondateur Closes #119

--- install/db_shemas/data.sql
+++ install/db_shemas/data.sql
@@ -215,7 +215,7 @@ INSERT INTO fsb2_groups (g_id, g_name, g_desc, g_type, g_hidden, g_color, g_open
 INSERT INTO fsb2_groups (g_id, g_name, g_desc, g_type, g_hidden, g_color, g_open, g_rank) VALUES (3, 'modo', '', 1, 0, 'class="modo"', 0, 0);
 INSERT INTO fsb2_groups (g_id, g_name, g_desc, g_type, g_hidden, g_color, g_open, g_rank) VALUES (4, 'modosup', '', 1, 0, 'class="modosup"', 0, 0);
 INSERT INTO fsb2_groups (g_id, g_name, g_desc, g_type, g_hidden, g_color, g_open, g_rank) VALUES (5, 'admin', '', 1, 0, 'class="admin"', 0, 0);
-INSERT INTO fsb2_groups (g_id, g_name, g_desc, g_type, g_hidden, g_color, g_open, g_rank) VALUES (6, 'Admin', '', 3, 0, '', 0, 0);
+INSERT INTO fsb2_groups (g_id, g_name, g_desc, g_type, g_hidden, g_color, g_open, g_rank) VALUES (6, 'founder', '', 3, 0, '', 0, 0);
 
 INSERT INTO fsb2_groups_auth (g_id, f_id, ga_view, ga_view_topics, ga_read, ga_create_post, ga_answer_post, ga_create_announce, ga_answer_announce, ga_edit, ga_delete, ga_moderator, ga_create_global_announce, ga_answer_global_announce) VALUES (1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 INSERT INTO fsb2_groups_auth (g_id, f_id, ga_view, ga_view_topics, ga_read, ga_create_post, ga_answer_post, ga_create_announce, ga_answer_announce, ga_edit, ga_delete, ga_moderator, ga_create_global_announce, ga_answer_global_announce) VALUES (2, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0);
