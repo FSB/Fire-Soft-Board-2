fb341133a53fd438d4707c054f07d189c7312529
Axel Fontana <fontana.axel@gmail.com>
2012-03-24 14:13:00 +0100

    Mise a jour des requetes ajax communes

--- main/javascript/common.js
+++ main/javascript/common.js
@@ -223,10 +223,10 @@ function trim(str)
 */
 function search_user(value, obj, id, id_field)
 {
-	var ajax = new Ajax(FSB_ROOT + 'ajax.' + FSB_PHPEXT,
+	var ajax = new Request(
 	{
-		method: 'get',
-		onComplete: function(txt, xml)
+		url: FSB_ROOT + 'ajax.' + FSB_PHPEXT + '?mode=search_user',
+		onSuccess: function(txt, xml)
 		{
 			if (!txt)
 			{
@@ -239,11 +239,9 @@ function search_user(value, obj, id, id_field)
 		}
 	});
 
-	ajax.request({
-		mode: 'search_user',
-		nickname: value,
-		jsid: id_field,
-		jsid2: id
+	ajax.send({
+		mode: 'get',
+        data: 'nickname=' + value + '&jsid=' + id_field + '&jsid2=' + id,
 	});
 }
 
